62bd622bc6e78a733b03ebe17dcb2009eee63289
Péter Kalicz <kaliczp@gmail.com>
2023-01-11 15:46:27 +0100

    Refine reading and factorise variable

--- irodalom.R
+++ irodalom.R
@@ -1,1 +1,7 @@
-irod <- read.csv2("Irod_gy.csv")
+irod <- read.csv2("Irod_gy.csv", fileEncoding = "utf8", skip = 1)
+fejléc <- read.csv2("Irod_gy.csv", nrow = 1, head = FALSE)
+fejléc[9:15] <- names(irod)[9:15]
+names(irod) <- fejléc
+
+## Faktorok
+jelleg <- factor(irod$`felmérés jellege`, levels = c("lokális", "regionális", "országos", "Európa", "globális"), ordered = TRUE)
